7c7ce250ce5579e929c2a85a7bc137ff6aa3e03a
Ken <73566770+jkenger@users.noreply.github.com>
2020-11-14 13:29:08 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Payroll_System
+{
+    public partial class Form1 : Form
+    {
+        salaryDeduction sd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox13_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int empId = Convert.ToInt32(tbEmpId.Text);
+            string empName = tbEmpName.Text.ToString();
+            int ratePerHour = Convert.ToInt32(tbRph.Text);
+            int hourPerDay = Convert.ToInt32(tbHpd.Text);
+            int numOfDays = Convert.ToInt32(tbNod.Text);
+
+            sd = new salaryDeduction(empId, empName, ratePerHour, hourPerDay, numOfDays); //Instatiate Personal Detail Class
+            //Console.WriteLine(sd.getGrossSalary());
+
+            //Set Value In Their Perspective Textbox
+            tbGrossSalary.Text = sd.getGrossSalary().ToString();
+            tbGrossSalary2.Text = sd.getGrossSalary().ToString();
+
+            tbTax.Text = sd.getTotalTax().ToString();
+            tbSSS.Text = sd.getTotalSSS().ToString();
+            tbPH.Text = sd.getTotalPhilHealth().ToString();
+
+            tbTotalDed.Text = sd.getTotalDeduction().ToString();
+            tbTotalDed2.Text = sd.getTotalDeduction().ToString();
+
+            tbNetSalary.Text = sd.getNetSalary().ToString();
+        }
+
+        private void tbClear_Click(object sender, EventArgs e)
+        {
+            tbEmpId.Text = "";
+            tbEmpName.Text = "";
+            tbRph.Text = "";
+            tbHpd.Text = "";
+            tbNod.Text = "";
+        }
+    }
+}
---
+++ PersonalDetails.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payroll_System
+{
+    public abstract class PersonalDetails
+    {
+        protected int employeeId;
+        protected string employeeName;
+        protected int ratePerHour;
+        protected int hourPerDay;
+        protected int noOfDays;
+        protected int grossSalary;
+        protected int netSalary;
+        public abstract void setGrossSalary(int rateHour, int hourDay, int numDays);
+        public abstract int getGrossSalary();
+        public abstract void setNetSalary();
+        public abstract int getNetSalary();
+        public abstract void setTotalDeduction(double tax, double sss, double philHealth);
+        public abstract int getTotalDeduction();
+
+        public PersonalDetails(int id, string name, int rateHour, int hourDay, int numDays)
+        {
+            //Get Personal Details
+            this.employeeId = id;
+            this.employeeName = name;
+            this.ratePerHour = rateHour;
+            this.hourPerDay = hourDay;
+            this.noOfDays = numDays;
+        }
+        
+    }
+
+    class salaryDeduction : PersonalDetails
+    {
+        private int totalDeduction;
+        private int totalGrossSalary;
+        private int totalNetSalary;
+        private int totalTax;
+        private int totalSSS;
+        private int totalPhilHealth;
+        public salaryDeduction(int id, string name, int rateHour, int hourDay, int numDays) : base(id, name, rateHour, hourDay, numDays)
+        {
+            double tax = .15;
+            double sss = .02;
+            double philHealth = .05;
+
+            setGrossSalary(rateHour, hourDay, numDays);
+            setTotalDeduction(tax, sss, philHealth);
+            setNetSalary();
+            
+        }
+
+        //Setter;
+        public override void setGrossSalary(int rateHour, int hourDay, int numDays)
+        {
+            totalGrossSalary = (rateHour * hourDay) * numDays;
+        }
+
+        public override void setTotalDeduction(double tax, double sss, double philHealth)
+        {
+            // Compute The Totals From Total Gross Salary
+            this.totalTax = (int)(totalGrossSalary * tax);
+            this.totalSSS = (int)(totalGrossSalary * sss);
+            this.totalPhilHealth = (int)(totalGrossSalary * philHealth);
+
+            this.totalDeduction = totalTax + totalSSS + totalPhilHealth; // Set The Total Deduction From Tax Sss And Philhealth
+
+        }
+
+        public override void setNetSalary()
+        {
+            this.totalNetSalary = this.totalGrossSalary - this.totalDeduction;
+        }
+
+        //Getter;
+        public override int getGrossSalary()
+        {
+            return this.totalGrossSalary;
+        }
+
+        public override int getTotalDeduction()
+        {
+            return this.totalDeduction;
+        }
+
+        public override int getNetSalary()
+        {
+            return this.totalNetSalary;
+        }
+
+        public int getTotalTax()
+        {
+            return this.totalTax;
+        }
+        public int getTotalSSS()
+        {
+            return this.totalSSS;
+        }
+        public int getTotalPhilHealth()
+        {
+            return this.totalPhilHealth;
+        }
+    }
+
+}
